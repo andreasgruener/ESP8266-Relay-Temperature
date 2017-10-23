d867dcc94732a7bec6a5dff1cf025a70597efe70
Andreas Grüner <andreas.gruener@mercatis.com>
2017-10-23 18:59:22 +0200

    changed tp DS18B20 from BMP180

--- src/ESP8266-Relay-Temperature.ino
+++ src/ESP8266-Relay-Temperature.ino
@@ -1,12 +1,10 @@
 #include <Homie.h>
-#include <Wire.h>
-#include <Adafruit_Sensor.h>
-#include <Adafruit_BMP280.h>
-#include <SPI.h>
+#include <OneWire.h> 
+#include <DallasTemperature.h>
 #include <U8g2lib.h>
 
 #define FW_NAME "Serverschrank"
-#define FW_VERSION "2.3"
+#define FW_VERSION "2.6"
 
 /* Magic sequence for Autodetectable Binary Upload */
 const char *__FLAGGED_FW_NAME = "\xbf\x84\xe4\x13\x54" FW_NAME "\x93\x44\x6b\xa7\x75";
@@ -15,9 +13,9 @@ const char *__FLAGGED_FW_VERSION = "\x6a\x3f\x3e\x0e\xe1" FW_VERSION "\xb0\x30\x
 
 // comment
 const int PIN_RELAY = 16; // D0;
-const int TEMPERATURE_INTERVAL = 60;
-const float TURN_OFF_FAN_TEMPERATURE = 32.0f;
-const float TURN_ON_FAN_TEMPERATURE = 33.0f;
+const int TEMPERATURE_INTERVAL = 2;
+const float TURN_OFF_FAN_TEMPERATURE = 33.0f;
+const float TURN_ON_FAN_TEMPERATURE = 35.0f;
 
 
 int temperature_interval = TEMPERATURE_INTERVAL;
@@ -27,7 +25,7 @@ unsigned long lastTemperatureSent = 0;
 unsigned long lastRetry = 0;
 int maxRetries = 5;
 int retries = 0;
-bool fanMode = false;
+bool fanIsRunning = false;
 bool fanAutomatic = true;
 
 float turnOffFanTemperature =  TURN_OFF_FAN_TEMPERATURE;
@@ -35,12 +33,20 @@ float turnOnFanTemperature = TURN_ON_FAN_TEMPERATURE;
 float minTemperature = 99.0f;
 float maxTemperature = -99.0f;
 float currentTemperature = -99.0f;
-float currentPressure = 0.0f;
 
 
 U8G2_SSD1306_64X48_ER_F_HW_I2C u8g2(U8G2_R0); // hardware
 
-#define SEALEVELPRESSURE_HPA (1013.25)
+#define ONE_WIRE_BUS 12       // D1
+#define ONE_WIRE_MAX_DEV 5 //The maximum number of devices
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature DS18B20(&oneWire);
+
+int numberOfDevices; //Number of temperature devices found
+DeviceAddress devAddr[ONE_WIRE_MAX_DEV];  //An array device temperature sensors
+float tempDev[ONE_WIRE_MAX_DEV]; //Saving the last measurement of temperature
+float tempDevLast[ONE_WIRE_MAX_DEV]; //Previous temperature measurement
+
 
 
 HomieNode lightNode("light", "switch");
@@ -48,11 +54,7 @@ HomieNode temperatureNode("temperature", "temperature");
 HomieNode fanNode("fan", "fan");
 HomieNode humidityNode("humidity", "humidity");
 
-HomieNode pressureNode("pressure", "pressure");
-HomieNode dewpointNode("dewpoint", "dewpoint");
 
-// Temperature Bosch BMP
-Adafruit_BMP280 bme;
 
 
 void setup() {
@@ -70,19 +72,37 @@ void setup() {
 
   Homie.setup();
 
-  bool success = bme.begin(0x76); //0x76); // DEFAULT S
-  if ( !success ) {
-    Serial.println("Could not find BME280 sensor!");
-  }
-  else {
-    Serial.println("Found BMP");
-  }
   u8g2.begin();
   u8g2.setFlipMode(true);
   u8g2.firstPage();
   do {
     u8g2.setFont(u8g2_font_u8glib_4_tf);
-    u8g2.drawStr(0,4,"Starting..");
+    u8g2.drawStr(0,4,"Scanning devices..");
+  } while( u8g2.nextPage() );
+  
+  // Start up the library
+  DS18B20.begin();
+  Serial.print("Found ");
+  int deviceCount = DS18B20.getDeviceCount();
+  char dc[5];
+  dtostrf(deviceCount, 2, 0, dc);
+
+  // bool success = bme.begin(0x76); //0x76); // DEFAULT S
+  // if ( !success ) {
+  //   Serial.println("Could not find BME280 sensor!");
+  // }
+  // else {
+  //   Serial.println("Found BMP");
+  // }
+  u8g2.begin();
+  u8g2.setFlipMode(true);
+  u8g2.firstPage();
+  do {
+    u8g2.setFont(u8g2_font_u8glib_4_tf);
+    u8g2.drawStr(0,14,"Starting..");
+    u8g2.drawStr(0,24,"Found");
+    u8g2.drawStr(30,24,dc);
+    u8g2.drawStr(40,24,"Devices");
   } while( u8g2.nextPage() );
 
 }
@@ -110,9 +130,6 @@ void setupHandler() {
   temperatureNode.advertise("degrees");
   temperatureNode.advertise("delay").settable(delayHandler);
   temperatureNode.setProperty("delay").send(String(temperature_interval));
-
-  pressureNode.setProperty("unit").send("hPa");
-  pressureNode.advertise("hPa");
 
 }
 
@@ -180,17 +197,17 @@ bool fanAutomaticHandler(const HomieRange& range, const String& value) {
 
 void setTurnOnTemperature(float temperature) {
   turnOnFanTemperature = temperature;
-  checkMeasurement(currentTemperature, currentPressure);
+  checkMeasurement(currentTemperature);
 }
 
 void setTurnOffTemperature(float temperature) {
   turnOffFanTemperature = temperature;
-  checkMeasurement(currentTemperature, currentPressure);
+  checkMeasurement(currentTemperature);
 }
 
 void setAutomaticFan(bool automatic) {
   fanAutomatic = automatic;
-  checkMeasurement(currentTemperature, currentPressure);
+  checkMeasurement(currentTemperature);
 }
 
 
@@ -198,13 +215,14 @@ void fanSwitch(bool on) {
   digitalWrite(PIN_RELAY, on ? HIGH : LOW);
   fanNode.setProperty("on").send((on ? "true" : "false"));
   Homie.getLogger() << "Relay is " << (on ? "on" : "off") << endl;
-  fanMode = on;
+  fanIsRunning = on;
   display();
 }
 
 void display() {
   char FAN_ON[9] = "Fan ON";
   char FAN_OFF[10] = "Fan OFF";
+ // char PRESSURE_TITLE[10] = "Luftdruck";
   char cTf[5];
   char cPf[5];
   char maxf[5];
@@ -214,7 +232,7 @@ void display() {
 
 
   dtostrf(currentTemperature, 2, 1, cTf);
-  dtostrf(currentPressure, 4, 0, cPf);
+
   dtostrf(maxTemperature, 2, 1, maxf);
   dtostrf(minTemperature, 2, 1, minf);
   dtostrf(turnOffFanTemperature, 2,0, turnOfff);
@@ -223,9 +241,9 @@ void display() {
   u8g2.firstPage();
   do {
     displaySmallRow(6,  minf, maxf);
-    display3SmallRow(14,  turnOfff, fanMode ? FAN_ON : FAN_OFF, turnOnf);
+    display3SmallRow(14,  turnOfff, fanIsRunning ? FAN_ON : FAN_OFF, turnOnf);
     displayRow(38, cTf);
-    displaySmallRow(48, "Luftdruck", cPf);
+   // displaySmallRow(48, PRESSURE_TITLE, cPf);
   } while ( u8g2.nextPage() );
   
 }
@@ -268,31 +286,87 @@ void displayRow(int row, char value[]) {
 /**
  * Check if fan limits are reached
  */
-void checkMeasurement(float temperature, float pressure) {
+void checkMeasurement(float temperature) {
 
   currentTemperature = temperature;
-  currentPressure = pressure;
 
   maxTemperature = currentTemperature > maxTemperature ? currentTemperature : maxTemperature;
   minTemperature = currentTemperature < minTemperature ? currentTemperature : minTemperature;
 
-  if (fanAutomatic && currentTemperature >= turnOnFanTemperature ) {
+  if (fanAutomatic && !fanIsRunning && currentTemperature >= turnOnFanTemperature ) {
     fanSwitch(true);
   }
-  if ( fanAutomatic && currentTemperature <= turnOffFanTemperature ) {
+  if ( fanAutomatic && fanIsRunning && currentTemperature <= turnOffFanTemperature ) {
     fanSwitch(false);
   }
 
 }
+//Convert device id to String
+String GetAddressToString(DeviceAddress deviceAddress){
+  String str = "";
+  for (uint8_t i = 0; i < 8; i++){
+    if( deviceAddress[i] < 16 ) str += String(0, HEX);
+    str += String(deviceAddress[i], HEX);
+  }
+  return str;
+}
+
+float checkTemperature() {
+  
+  Serial.print("Parasite power is: "); 
+  if( DS18B20.isParasitePowerMode() ){ 
+    Serial.println("ON");
+  }else{
+    Serial.println("OFF");
+  }
+  
+    // Loop through each device, print out address
+    int numberOfDevices = DS18B20.getDeviceCount();
+    Serial.print( "Device count: " );
+    Serial.println( numberOfDevices );
+    DS18B20.requestTemperatures();
+
+    for(int i=0;i<numberOfDevices; i++){
+      // Search the wire for address
+      if( DS18B20.getAddress(devAddr[i], i) ){
+        //devAddr[i] = tempDeviceAddress;
+        Serial.print("Found device ");
+        Serial.print(i, DEC);
+        Serial.print(" with address: " + GetAddressToString(devAddr[i]));
+        Serial.println();
+      } else {
+        Serial.print("Found ghost device at ");
+        Serial.print(i, DEC);
+        Serial.print(" but could not detect address. Check power and cabling");
+      }
+  
+      //Get resolution of DS18b20
+      Serial.print("Resolution: ");
+      Serial.print(DS18B20.getResolution( devAddr[i] ));
+      Serial.println();
+  
+      //Read temperature from DS18b20
+      float tempC = DS18B20.getTempC( devAddr[i] );
+      Serial.print("Temp C: ");
+      Serial.println(tempC); 
+      if (i == 0) {
+        return tempC;
+      }
+      delay(100);
+    }
+    return 0;
+  
+  }
 
 
 void checkSensors() {
+
+ 
   
   if (  ( retries < maxRetries && millis() - lastRetry >= 3000UL ) ||
         millis() - lastTemperatureSent >= temperature_interval * 1000UL ) {
 
     float temperature = 0;
-    float pressure = 0;
     lastTemperatureSent =  millis();
 
     // successful the lasttime, new game
@@ -306,8 +380,9 @@ void checkSensors() {
     }
 
     // try getting temperature -- sometimes there is no answer from the sensor
-    temperature = bme.readTemperature();
-    //Homie.getLogger() << "Temperature reading " << temperature << endl;
+    //temperature = bme.readTemperature();
+    temperature =  checkTemperature();
+    Homie.getLogger() << "Temperature reading " << temperature << endl;
     if ( isnan(temperature) || temperature == 0 ) {
       retries++;
       Homie.getLogger() << "*** Temperature not available retrying " << retries << ". time " << " max Retries = " << lastRetry << "... " << millis() << endl;
@@ -319,17 +394,13 @@ void checkSensors() {
       Homie.getLogger() << "Temperature: " << temperature << " °C" << endl;
       temperatureNode.setProperty("degrees").send(String(temperature));
 
-      pressure = bme.readPressure() / 100.0f;  // Pascal --> hPa
-      Homie.getLogger() << "Pressure: " << pressure << " hPa" << endl;
-      pressureNode.setProperty("hPa").send(String(pressure));
-
       temperatureNode.setProperty("sensor_ok").send("1");
 
       lastTemperatureSent = millis();
       //lastRetry = millis();
       retries = maxRetries + 1;
       
-      checkMeasurement(temperature, pressure);
+      checkMeasurement(temperature);
     }
     display();
   }
@@ -341,7 +412,8 @@ void checkSensors() {
 
 void loopHandler() {
   //Homie.getLogger() << "## Temperature: " << checkGY906Temperature() << " C" << endl;
-  checkSensors();
+   checkSensors();
+  
 }
 
 void loop() {
